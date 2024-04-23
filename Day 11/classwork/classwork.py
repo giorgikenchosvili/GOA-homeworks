8bb11cd0af444828fe504ebbf2fcce0dcbba81bc
giorgikenchosvili <gkenchoshvili63@gmail.com>
2024-04-23 22:55:03 +0400

    commit

---
+++ Day 11/classwork/classwork.py
@@ -0,0 +1,12 @@
+num1=int(input("pls enter any number"))
+num2=int(input("pls enter any number"))
+num3=int(input("pls enter any number"))
+
+if num1==10  :
+     print(num1 + num2)
+
+elif num2==20 :
+     print(num2 + num3)
+
+elif num3==15 :
+     print(num3 + num1)
---
+++ Day 12/howmork/homwork.py
@@ -0,0 +1,21 @@
+for i  in range(0, 10+1):
+     if i % 2 ==1:
+       print(i)
+
+
+
+num =int(input("enter any number: "))
+
+for i in range(0, num+1) :
+     print(i)
+
+
+sum=0
+
+for i in range(0, 50+1):
+    sum +=i
+    print(sum)
+
+for i in range(1, 50 + 1):
+    if i % 5 ==0 :
+        print(i)
