4f11f3f44e95359f40eff95a659f3648be8df9e3
giorgikenchosvili <gkenchoshvili63@gmail.com>
2024-05-27 22:03:48 +0400

    commit

---
+++ Day 16/classwork/classwork.py
@@ -0,0 +1,15 @@
+# animes ={"onepice","sololeveling","reicornishen to slime",""}
+
+
+
+
+cars ={}
+
+for i in range(5):
+    car=input("pls enter your car name")
+    cars.append(car)
+
+
+
+print(cars)
+
